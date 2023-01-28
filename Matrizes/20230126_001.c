0935c087580a4b6b35aec0acfc3f257a5f7341d9
Juliene Witória Viana <119975669+Jojobinea@users.noreply.github.com>
2023-01-27 20:33:06 -0400

    Update 20230126_001.c

--- Matrizes/20230126_001.c
+++ Matrizes/20230126_001.c
@@ -1,20 +1,9 @@
+
 #include <stdio.h>
 
 int main(){
-    int m1[3][5], m2[3][5];
-
-    printf("Informe os valores da primeira matriz: \n");
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 3; j++){
-            scanf("%d", &m1[i][j]);
-        }
-    }
-    printf("Informe os valores da segunda matriz: \n");
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 3; j++){
-            scanf("%d", &m2[i][j]);
-        }
-    }
+    int m1[5][3] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4}, {5, 5, 5}};
+    int m2[5][3] = {{6, 6, 6}, {7, 7, 7}, {8, 8, 8}, {9, 9, 9}, {10, 10, 10}};
 
     printf("\nMatriz 1:\n");
     for(int i = 0; i < 5; i++){
