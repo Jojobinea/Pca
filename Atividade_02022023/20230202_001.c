9f08d6d323ce7316495e89fb5b719651705c5cc4
Juliene Witória Viana <119975669+Jojobinea@users.noreply.github.com>
2023-02-03 23:56:35 -0400

    Add files via upload

---
+++ Atividade_02022023/20230202_001.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define Equipes 5
+#define Alunos 4
+
+int main()
+{
+    char alunos[Equipes][Alunos][120];
+    char consultar, pesquisar[120], sobrenome[60];
+    int numEquipe;
+
+    for(int i = 0; i < Equipes; i++){
+        printf("Informe os Alunos da Equipe %d:\n", i+1);
+
+        for(int j = 0; j < Alunos; j++){
+            printf("%do Aluno - Primeiro Nome: ", j+1);
+            scanf("%s", alunos[i][j]);
+            printf("%do Aluno - Sobrenome: ", j+1);
+            scanf("%s", sobrenome);
+            strcat(alunos[i][j], " ");
+            strcat(alunos[i][j], sobrenome);
+        }
+        printf("\n");
+    }
+
+    printf("Deseja consultar a equipe de um aluno? S/N\n");
+    scanf(" %c", &consultar);
+
+    while(toupper(consultar)=='S'){
+        printf("Insira o nome de um aluno:\n");
+        scanf("%s", pesquisar);
+        printf("Insira o sobrenome do aluno: ");
+        scanf("%s", sobrenome);
+        strcat(pesquisar, " ");
+        strcat(pesquisar, sobrenome);
+        numEquipe = 0;
+
+        for(int i = 0; i < Equipes; i++){
+            for(int j = 0; j < Alunos; j++){
+                if(strcmp(alunos[i][j], pesquisar) == 0){
+                    numEquipe = i+1;
+                    break;
+                }
+            }
+        }
+
+        if(numEquipe!=0)
+            printf("%s eh da equipe %d\n", pesquisar, numEquipe);
+        else
+            printf("Aluno nao encontrado\n");
+
+        printf("Deseja consultar de outro aluno? S/N\n");
+        scanf(" %c", &consultar);
+    }
+    return 0;
+}
